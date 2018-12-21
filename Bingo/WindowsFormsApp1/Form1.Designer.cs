bf2e1e20e85269d7c7afb2afe041aba1e6244d41
Alexa Monge Castro <alex.monge64@hotmail.com>
2018-12-20 21:42:46 -0600

    Modificación de interfaz

--- Bingo/WindowsFormsApp1/Form1.Designer.cs
+++ Bingo/WindowsFormsApp1/Form1.Designer.cs
@@ -42,7 +42,7 @@
             this.cbPlayer = new System.Windows.Forms.ComboBox();
             this.cbCarton = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lbElegir = new System.Windows.Forms.Label();
             this.btnTerminar = new System.Windows.Forms.Button();
             this.dgvCarton = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCarton)).BeginInit();
@@ -57,30 +57,27 @@
             "Jugador 3",
             "Jugador 4",
             "Jugador 5"});
-            this.cmbJugadores.Location = new System.Drawing.Point(39, 96);
-            this.cmbJugadores.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbJugadores.Location = new System.Drawing.Point(29, 78);
             this.cmbJugadores.Name = "cmbJugadores";
-            this.cmbJugadores.Size = new System.Drawing.Size(160, 24);
+            this.cmbJugadores.Size = new System.Drawing.Size(121, 21);
             this.cmbJugadores.TabIndex = 0;
             this.cmbJugadores.SelectedIndexChanged += new System.EventHandler(this.cmbJugadores_SelectedIndexChanged);
             // 
             // Jugadores
             // 
             this.Jugadores.AutoSize = true;
-            this.Jugadores.Location = new System.Drawing.Point(73, 74);
-            this.Jugadores.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Jugadores.Location = new System.Drawing.Point(55, 60);
             this.Jugadores.Name = "Jugadores";
-            this.Jugadores.Size = new System.Drawing.Size(75, 17);
+            this.Jugadores.Size = new System.Drawing.Size(56, 13);
             this.Jugadores.TabIndex = 1;
             this.Jugadores.Text = "Jugadores";
             // 
             // Modalidad
             // 
             this.Modalidad.AutoSize = true;
-            this.Modalidad.Location = new System.Drawing.Point(619, 74);
-            this.Modalidad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Modalidad.Location = new System.Drawing.Point(464, 60);
             this.Modalidad.Name = "Modalidad";
-            this.Modalidad.Size = new System.Drawing.Size(136, 17);
+            this.Modalidad.Size = new System.Drawing.Size(103, 13);
             this.Modalidad.TabIndex = 3;
             this.Modalidad.Text = "Modalidad de Juego";
             // 
@@ -94,10 +91,9 @@
             "Letra X",
             "Letra O",
             "Letra U"});
-            this.cmbModalidad.Location = new System.Drawing.Point(608, 96);
-            this.cmbModalidad.Margin = new System.Windows.Forms.Padding(4);
+            this.cmbModalidad.Location = new System.Drawing.Point(456, 78);
             this.cmbModalidad.Name = "cmbModalidad";
-            this.cmbModalidad.Size = new System.Drawing.Size(160, 24);
+            this.cmbModalidad.Size = new System.Drawing.Size(121, 21);
             this.cmbModalidad.TabIndex = 4;
             this.cmbModalidad.Tag = "";
             this.cmbModalidad.SelectedIndexChanged += new System.EventHandler(this.cmbModalidad_SelectedIndexChanged);
@@ -111,19 +107,17 @@
             "3",
             "4",
             "5"});
-            this.cbCartxJu.Location = new System.Drawing.Point(244, 96);
-            this.cbCartxJu.Margin = new System.Windows.Forms.Padding(4);
+            this.cbCartxJu.Location = new System.Drawing.Point(183, 78);
             this.cbCartxJu.Name = "cbCartxJu";
-            this.cbCartxJu.Size = new System.Drawing.Size(160, 24);
+            this.cbCartxJu.Size = new System.Drawing.Size(121, 21);
             this.cbCartxJu.TabIndex = 7;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(255, 74);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(191, 60);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 17);
+            this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 8;
             this.label1.Text = "Cartones x jugador";
             // 
@@ -131,20 +125,18 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(36, 16);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(27, 13);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(283, 29);
+            this.label2.Size = new System.Drawing.Size(221, 25);
             this.label2.TabIndex = 9;
             this.label2.Text = "Seleccione cantidad de:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(437, 74);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(328, 60);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(121, 17);
+            this.label3.Size = new System.Drawing.Size(91, 13);
             this.label3.TabIndex = 10;
             this.label3.Text = "Numeros x Carton";
             // 
@@ -163,18 +155,16 @@
             "65",
             "70",
             "75"});
-            this.cbNumxCart.Location = new System.Drawing.Point(427, 94);
-            this.cbNumxCart.Margin = new System.Windows.Forms.Padding(4);
+            this.cbNumxCart.Location = new System.Drawing.Point(320, 76);
             this.cbNumxCart.Name = "cbNumxCart";
-            this.cbNumxCart.Size = new System.Drawing.Size(160, 24);
+            this.cbNumxCart.Size = new System.Drawing.Size(121, 21);
             this.cbNumxCart.TabIndex = 11;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(407, 353);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(305, 287);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 12;
             this.button1.Text = "Mostrar";
             this.button1.UseVisualStyleBackColor = true;
@@ -182,10 +172,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(801, 94);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Location = new System.Drawing.Point(601, 76);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 13;
             this.button2.Text = "Play";
             this.button2.UseVisualStyleBackColor = true;
@@ -194,47 +183,42 @@
             // cbPlayer
             // 
             this.cbPlayer.FormattingEnabled = true;
-            this.cbPlayer.Location = new System.Drawing.Point(667, 190);
-            this.cbPlayer.Margin = new System.Windows.Forms.Padding(4);
+            this.cbPlayer.Location = new System.Drawing.Point(500, 154);
             this.cbPlayer.Name = "cbPlayer";
-            this.cbPlayer.Size = new System.Drawing.Size(160, 24);
+            this.cbPlayer.Size = new System.Drawing.Size(121, 21);
             this.cbPlayer.TabIndex = 14;
             // 
             // cbCarton
             // 
             this.cbCarton.FormattingEnabled = true;
-            this.cbCarton.Location = new System.Drawing.Point(667, 277);
-            this.cbCarton.Margin = new System.Windows.Forms.Padding(4);
+            this.cbCarton.Location = new System.Drawing.Point(500, 225);
             this.cbCarton.Name = "cbCarton";
-            this.cbCarton.Size = new System.Drawing.Size(160, 24);
+            this.cbCarton.Size = new System.Drawing.Size(121, 21);
             this.cbCarton.TabIndex = 15;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(667, 254);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(500, 206);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 16;
             this.label4.Text = "label4";
             // 
-            // label5
+            // lbElegir
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(671, 166);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(46, 17);
-            this.label5.TabIndex = 17;
-            this.label5.Text = "label5";
+            this.lbElegir.AutoSize = true;
+            this.lbElegir.Location = new System.Drawing.Point(503, 135);
+            this.lbElegir.Name = "lbElegir";
+            this.lbElegir.Size = new System.Drawing.Size(74, 13);
+            this.lbElegir.TabIndex = 17;
+            this.lbElegir.Text = "Elegir Jugador";
             // 
             // btnTerminar
             // 
-            this.btnTerminar.Location = new System.Drawing.Point(727, 353);
-            this.btnTerminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnTerminar.Location = new System.Drawing.Point(545, 287);
             this.btnTerminar.Name = "btnTerminar";
-            this.btnTerminar.Size = new System.Drawing.Size(149, 28);
+            this.btnTerminar.Size = new System.Drawing.Size(112, 23);
             this.btnTerminar.TabIndex = 18;
             this.btnTerminar.Text = "terminar Juego";
             this.btnTerminar.UseVisualStyleBackColor = true;
@@ -243,22 +227,23 @@
             // dgvCarton
             // 
             this.dgvCarton.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCarton.Location = new System.Drawing.Point(107, 154);
+            this.dgvCarton.Location = new System.Drawing.Point(80, 125);
+            this.dgvCarton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvCarton.Name = "dgvCarton";
             this.dgvCarton.RowTemplate.Height = 24;
-            this.dgvCarton.Size = new System.Drawing.Size(400, 163);
+            this.dgvCarton.Size = new System.Drawing.Size(300, 132);
             this.dgvCarton.TabIndex = 19;
             this.dgvCarton.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(929, 416);
+            this.ClientSize = new System.Drawing.Size(697, 338);
             this.Controls.Add(this.dgvCarton);
             this.Controls.Add(this.btnTerminar);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lbElegir);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.cbCarton);
             this.Controls.Add(this.cbPlayer);
@@ -273,7 +258,6 @@
             this.Controls.Add(this.Modalidad);
             this.Controls.Add(this.Jugadores);
             this.Controls.Add(this.cmbJugadores);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "BINGO";
             ((System.ComponentModel.ISupportInitialize)(this.dgvCarton)).EndInit();
@@ -298,7 +282,7 @@
         private System.Windows.Forms.ComboBox cbPlayer;
         private System.Windows.Forms.ComboBox cbCarton;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbElegir;
         private System.Windows.Forms.Button btnTerminar;
         private System.Windows.Forms.DataGridView dgvCarton;
     }
